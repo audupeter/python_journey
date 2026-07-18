a6fc4fde322e8d110f7b9d8f0fa2fb6eec0f5a3a
Peter Audu <audupeter17@gmail.com>
2026-07-18 01:07:10 +0100

    Created First Project

---
+++ Projects/future_ai_engineer.py
@@ -0,0 +1,27 @@
+print("=" * 40)
+print("         FUTURE AI ENGINEER PROFILE")
+print("=" * 40)
+
+name = input("What is your name? ")
+print("Name:",name)
+
+age = int (input("How old are you? "))
+print("Age:", age)
+
+
+country = input("What country are you from? ")
+print("Country:", country)
+
+dream_job = input("What's your dream job? ")
+print("Dream Job:", dream_job)
+
+skill = input("What skill are you learning? ")
+print("Skill:", skill)
+
+dream_income = input("What's your dream annual income? ")
+print("Dream Income:", dream_income)
+
+future_company = input("What company would you like to build one day? ")
+print("Future Company:", future_company)
+
+print("Note:\n", name + ", remember: \n Success is built one day at a time.")
