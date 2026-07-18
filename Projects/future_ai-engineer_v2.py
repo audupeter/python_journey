5723a89dcbf6961efe4def5f065c7bdf8955806b
Peter Audu <audupeter17@gmail.com>
2026-07-18 01:36:17 +0100

    Modified Future AI Engineer Project

---
+++ Projects/future_ai-engineer_v2.py
@@ -0,0 +1,30 @@
+print("=" * 40)
+print("         FUTURE AI ENGINEER PROFILE")
+print("=" * 40)
+
+name = input("What is your name? ")
+
+age = int (input("How old are you? "))
+
+country = input("What country are you from? ")
+
+dream_job = input("What's your dream job? ")
+
+skill = input("What skill are you learning? ")
+
+dream_income = input("What's your dream annual income? ")
+
+future_company = input("What company would you like to build one day? ")
+
+print("=" * 40)
+print("         FUTURE AI ENGINEER PROFILE")
+print("=" * 40)
+
+print("Name:",name)
+print("Age:", age)
+print("Country:", country)
+print("Dream Job:", dream_job)
+print("Skill:", skill)
+print("Dream Income:", dream_income)
+print("Future Company:", future_company)
+print("Note:\n", name + ", remember: \n Success is built one day at a time.")
