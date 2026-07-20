a449ba41ee767101d2a6a488acfab0b8aac46e85
Peter Audu <audupeter17@gmail.com>
2026-07-21 00:00:55 +0100

    Added University Admission Checker project

---
+++ Projects/university_admission_checker.py
@@ -0,0 +1,41 @@
+print("="*40)
+print("           UNIVERSITY ADMISSION CHECKER")
+print("="*40)
+
+name = input("Enter your name: ")
+age = int(input("How old are you?: "))
+score = int(input("Enter JAMB score: "))
+department = input("Enter Desired Department: ")
+
+print("Student Name: ", name)
+print("Age: ", age)
+print("JAMB Score: ", score)
+print()
+if score >= 280:
+    print("Excellent!")
+    print("High chance of Admission.")
+    print()
+elif score >= 200:
+    print("Good score.")
+    print("You may get admission.")
+    print()
+else:
+    print("Unfortunately,")
+    print("you did not meet the cut-off mark.") 
+    print()
+
+if age >= 18:
+    print("You meet the age requirement.")
+    print()
+else:
+    print("Below recommended admission age.")
+    print()
+
+if department == "Medicine":
+    print("Required Score: 300")
+elif department == "Engineering":
+    print("Required Score: 250")
+elif department == "Computer Science":
+    print("Required Score: 250")
+else: 
+    print("General admission requirement rules apply.")
