927be5e8f6531e470cb37d9209b1e6180f70b74d
audupeter <84791492+audupeter@users.noreply.github.com>
2026-07-24 16:26:55 +0100

    created Area Calculator

---
+++ Projects/area_calculator.py
@@ -0,0 +1,12 @@
+def rectangle_area(length,width):
+    return(length * width)
+
+area = rectangle_area(10, 5)
+print(area)
+
+def square_area(side):
+    return(side**2)
+
+area = square_area(4)
+print(area)
+
