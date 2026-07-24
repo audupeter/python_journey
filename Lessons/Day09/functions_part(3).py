b59bf0409c3f8ef31c40c4ef931731c9739ce764
audupeter <84791492+audupeter@users.noreply.github.com>
2026-07-24 16:09:47 +0100

    Day 9 function part 3

---
+++ Lessons/Day09/functions_part(3).py
@@ -0,0 +1,19 @@
+def add(a,b):
+    return(a + b)
+
+answer = add(10, 5)
+
+print(answer)
+
+total = add(100, 50)
+tax = 20
+
+print(total - tax)
+
+def square(number):
+    return (number * number)
+
+result = square(8)
+print(result)
+
+print(square(12))
