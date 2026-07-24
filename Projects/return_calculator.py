91af38a1e8d5186a432d3ee3680901c583ef74f6
audupeter <84791492+audupeter@users.noreply.github.com>
2026-07-24 16:17:43 +0100

    Created return calculator project

---
+++ Projects/return_calculator.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return(a + b)
+
+def subtract(a, b):
+    return(a - b)
+
+def multiply(a, b):
+    return(a * b)
+
+def divide(a, b):
+    return(a / b)
+
+answer = add(10, 5)
+print(answer)
+
+answer = subtract(50, 20)
+print(answer)
+
+answer = multiply(20, 5)
+print(answer)
+
+answer = divide(100, 50)
+print(answer)
