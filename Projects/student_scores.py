7948b8f8fe916fc10a3e95b855fbc8a3664b4bc2
audupeter <84791492+audupeter@users.noreply.github.com>
2026-07-25 21:50:21 +0100

    created Student_scores.py

---
+++ Projects/student_scores.py
@@ -0,0 +1,13 @@
+scores = [78, 90, 65, 88, 95]
+print(scores)
+print(scores[0])
+print(scores[2])
+print(scores[4])
+print(len(scores))
+print()
+own_list = [2, 4, 6, 8, 10]
+print(own_list)
+print(own_list[0])
+print(own_list[3])
+print(own_list[4])
+print(len(own_list))
