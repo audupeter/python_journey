2c9e02bad24e726894c9d98be64f208c903091a3
audupeter <84791492+audupeter@users.noreply.github.com>
2026-07-25 21:42:56 +0100

    Created favourite_things.py

---
+++ Projects/favourite_things.py
@@ -0,0 +1,18 @@
+foods = ["Jollof Rice", "Pasta", "Chicken Curry"]
+football_clubs = ["Manchester United", "Barcelona"]
+programming_languages = ["Python", "Js", "MySQL"]
+
+print(foods)
+print(foods[0])
+print(foods[2])
+print(len(foods))
+print()
+print(football_clubs)
+print(football_clubs[0])
+print(football_clubs[1])
+print(len(football_clubs))
+print()
+print(programming_languages)
+print(programming_languages[0])
+print(programming_languages[2])
+print(len(programming_languages))
