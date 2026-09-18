0d96bf67a41be08f91d52c8ba67bc7cbd2147422
audupeter <84791492+audupeter@users.noreply.github.com>
2026-09-18 12:03:07 +0100

    modified banking_program

--- Projects/banking_program.py
+++ Projects/banking_program.py
@@ -4,24 +4,25 @@ print("                 BANKING PROGRAM")
 print("="*50)
 
 def show_menu():
+    print("Welcome!")
     print("1. Check balance")
     print("2. Deposit")
     print("3. Withdrawal")
     print("4. Exit")
 
 def check_balance(balance):
-    return (balance)
+    return balance
 
 def deposit(balance, amount ):
-    return(balance + amount)
+    return balance + amount
 
 def withdrawal(balance, amount):
     if amount > balance:
         print("Insufficient Funds.")
-        return (balance)
+        return balance
     else:
         print("Withdrawal Successful!")
-    return(balance - amount)
+    return balance - amount
 
 running = True
 while running:
@@ -32,7 +33,7 @@ while running:
     elif choice == 2:
         amount = int(input("How much do you want to deposit? "))
         balance = deposit(balance, amount)
-        print("Deposit succesfull! your balance is:", balance)
+        print("Deposit successful! your balance is:", balance)
     elif choice == 3:
         amount = int(input("How much do you want to withdraw? "))
         balance = withdrawal(balance, amount)
@@ -40,3 +41,5 @@ while running:
     elif choice == 4:
         print("Goodbye!")
         running = False
+    else:
+        print("Invalid Option. Enter a valid option.")
